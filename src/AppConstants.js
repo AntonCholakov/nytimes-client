4dc04a2c1842161c88699c5964027d3bded43acf
Anton Cholakov <anton@pliant.io>
2019-06-17 16:15:25 +0300

    Add API KEY

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -1,2 +1,2 @@
-const API_KEY = '';
+const API_KEY = 'y14Lnciv4BLNWAqXGu7l3dZzIc7NGz9D'; // TODO Replace this API KEY with your own
 export const API_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=${API_KEY}`;
